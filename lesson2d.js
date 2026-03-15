4290ec0e8e30e74e352bdaf8514ef8e8f70fa3c8
Gabriel-by34 <museradotcom04@gmail.com>
2026-03-15 21:48:33 +0300

    first commit

---
+++ lesson2d.js
@@ -0,0 +1,14 @@
+// if...else...if else statement,// it used to evaluate multiple statements
+let age=14
+if(age<15){
+    console.log("You are too young to watch this episode.")
+}
+else if(age>=15 && age<=18){
+    console.log("You can watch this episode under parental guidence")
+}
+else if(age>18&& age <40){
+    console.log("sit and enjoy the Episode")
+}
+else{
+    console.log("this episode is not suitable for you.")
+}
